0426b3b78b3b8cbc67abddfc9d5dc20fff327ed4
Dillon Morris <dmorris@elmstreettechnology.com>
2018-10-26 14:25:58 -0700

    Introduction and Resources built out

--- src/components/storm/Hero.js
+++ src/components/storm/Hero.js
@@ -5,7 +5,28 @@ import withStyles from "elevate-ui/withStyles";
 const Hero = ({ classes, className }) => (
   <div className={classNames(classes.root, className)}>
     <div className={classes.container}>
-      <h1>Hello world</h1>
+      <div className={classes.heroLeft}>
+        <h1 className={classes.heading}>
+          We want to <span className={classes.headingEnd}>help.</span>
+        </h1>
+        <p className={classes.subHeading}>
+          Everything we know about storms is at your disposal.
+        </p>
+        <p className={classes.headingBottom}>
+          <span className={classes.headingBottomTop}>Stay informed.</span>
+        </p>
+        <p className={classes.headingBottom}>Keep you and your family safe.</p>
+      </div>
+      <div className={classes.heroRight}>
+        <video
+          controls
+          src="https://youtu.be/fSTdAzcQIB8"
+          width="560"
+          height="360"
+        >
+          Sorry, your browser doesn't support embedded videos.
+        </video>
+      </div>
     </div>
   </div>
 );
@@ -16,10 +37,42 @@ const styles = (theme) => ({
     width: "100%",
   },
   container: {
+    display: "flex",
+    justifyContent: "space-between",
     padding: "0px 14px 0px",
     maxWidth: "1080px",
     width: "100%",
     margin: "auto",
+  },
+  heroLeft: {
+    paddingTop: "20px",
+  },
+  heading: {
+    color: theme.colors.black,
+    fontFamily: "League Spartan",
+    fontSize: "34px",
+    paddingBottom: "16px",
+  },
+  headingEnd: {
+    color: theme.colors.primary,
+  },
+  subHeading: {
+    color: theme.colors.gray600,
+    fontFamily: "League Spartan",
+    fontSize: "18px",
+    lineHeight: "1.5",
+    maxWidth: "320px",
+    paddingBottom: "80px",
+  },
+  headingBottom: {
+    color: theme.colors.gray600,
+    fontFamily: "League Spartan",
+    fontSize: "18px",
+    lineHeight: "1.5",
+    maxWidth: "320px",
+  },
+  headingBottomTop: {
+    color: theme.colors.black,
   },
 });
 
---
+++ src/components/storm/Introduction.js
@@ -0,0 +1,104 @@
+import React from "react";
+import classNames from "classnames";
+import withStyles from "elevate-ui/withStyles";
+
+const Introduction = ({ classes, className }) => (
+  <div className={classNames(classes.root, className)}>
+    <div className={classes.container}>
+      <div className={classes.inner}>
+        <h1 className={classes.heading}>Why We're here</h1>
+        <p className={classes.paragraph}>
+          We are all impacted by natural disasters, whether directly or
+          indirectly. <span className={classes.bold}>Storm Questions</span>{" "}
+          wants to provide a resource for you to ask questions and get answers.
+          We also provide a list of resources to help you get through a storm’s
+          aftermath.
+        </p>
+        <p className={classes.paragraph}>
+          We are aggregating{" "}
+          <span className={classes.bold}>industry experts</span> including
+          attorneys, public insurance adjusters, contractors, roofers,
+          legislators, members from offices of emergency management, FEMA
+          directors, etc.
+        </p>
+        <p className={classes.paragraph}>
+          The purpose of this site is to provide everyone that experienced storm
+          damage or related business issues with a forum where you may ask
+          questions and get the{" "}
+          <span style={{ fontStyle: "italic" }}>most accurate</span>, up-to-date{" "}
+          answers from the people that know best.
+        </p>
+        <p className={classes.paragraph}>
+          Please <span className={classes.underline}>check back often</span> as
+          we will also be adding information for where you can obtain loans and
+          financial resources as you make repairs and recover from the storm.
+        </p>
+        <p className={classes.paragraph}>
+          In addition to resources and information for homeowners, business
+          owners will also have information to assist in learning more about
+          business interruption, loss of use, etc., and other issues that impact
+          your revenue and your business.
+        </p>
+        <h3 className={classes.closing}>
+          Contact us anytime at{" "}
+          <a href="mailto:AllStormQuestions@gmail.com" className={classes.link}>
+            AllStormQuestions@gmail.com
+          </a>
+        </h3>
+      </div>
+    </div>
+  </div>
+);
+
+const styles = (theme) => ({
+  root: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "center",
+  },
+  container: {
+    display: "flex",
+    padding: "80px 0px",
+    maxWidth: "680px",
+  },
+  heading: {
+    color: theme.colors.black,
+    fontFamily: "League Spartan",
+    fontSize: "26px",
+    paddingBottom: "18px",
+  },
+  inner: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  paragraph: {
+    color: theme.colors.gray600,
+    fontWeight: "400",
+    fontSize: "21px",
+    lineHeight: "1.4",
+    paddingBottom: "28px",
+  },
+  bold: {
+    fontWeight: "700",
+  },
+  underline: {
+    borderBottom: `2px solid ${theme.colors.primary}`,
+  },
+  closing: {
+    color: theme.colors.black,
+    fontWeight: "700",
+    fontSize: "21px",
+  },
+  link: {
+    color: theme.colors.primary,
+    textDecoration: "none",
+    fontWeight: "700",
+
+    "&:hover": {
+      borderBottom: `2px solid ${theme.colors.gray600}`,
+      transition: "250ms all",
+    },
+  },
+});
+
+export default withStyles(styles)(Introduction);
